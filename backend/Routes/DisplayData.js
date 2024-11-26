aa79e5fd2df5d57f41bb23595e3d2dd71237755f
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2024-11-26 19:09:13 +0530

    Update DisplayData.js

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.post("/foodData", async(req, res) => {
   try {
     // console.log(global.food_items);
-   await res.send([global.food_items, global.foodCategory]);
+    res.send([global.food_items, global.foodCategory]);
   } catch (error) {
     console.error(error.message);
     res.send("Server error");
